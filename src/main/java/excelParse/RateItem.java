f886cae3afb2ec5ff43d221c907eea49a9534ed5
AtanvarnoJack <alexcornillon@gmail.com>
2015-02-17 10:01:17 +0100

    Rajout de l'objet RateItem. Comprenant un RateKey et un RateValue

---
+++ src/main/java/excelParse/RateItem.java
@@ -0,0 +1,46 @@
+package excelParse;
+
+import java.util.Objects;
+
+/**
+ * Created by alexandre on 16/02/2015.
+ */
+public class RateItem {
+    private RateKey rateKey;
+    private RateValue rateValue;
+
+    public RateKey getRateKey() {
+        return rateKey;
+    }
+
+    public void setRateKey(RateKey rateKey) {
+        this.rateKey = rateKey;
+    }
+
+    public RateValue getRateValue() {
+        return rateValue;
+    }
+
+    public void setRateValue(RateValue rateValue) {
+        this.rateValue = rateValue;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(rateKey, rateValue);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        final RateItem other = (RateItem) obj;
+        return Objects.equals(this.rateKey, other.rateKey)
+                && Objects.equals(this.rateValue, other.rateValue);
+    }
+
+}
