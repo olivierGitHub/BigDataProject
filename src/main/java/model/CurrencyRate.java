a941cbcef373be43e77e64f51dacd7b0f38d988d
AtanvarnoJack <alexcornillon@gmail.com>
2015-02-17 11:03:35 +0100

    Ajout d'une enum private comportant les Symbole et ajout dans la méthode set currency code de la récupération du symbole par l'enum.

--- src/main/java/model/CurrencyRate.java
+++ src/main/java/model/CurrencyRate.java
@@ -3,6 +3,7 @@ package model;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import java.util.Objects;
 
 /**
  * Created by Arnaud on 12/02/2015.
@@ -15,4 +16,78 @@ public class CurrencyRate {
     private int id;
     private String currencyName;
     private String currencyCode;
+
+    /***
+     * Private enum for set currencyCode with currencyName*
+     */
+    private enum unitySymbol {
+        Dollar("$"),
+        Euro("€"),
+        Livre("£"),
+        Peso("C"),
+        Couronne("K"),
+        Sheqel("₪"),
+        Yen("¥"),
+        Won("₩"),
+        Zloty("z"),
+        Tolar("tolars"),
+        Franc("F"),
+        Lire("T");
+
+        private String symbol = "";
+        //Constructeur
+        unitySymbol(String name){
+            this.symbol = name;
+        }
+        public String toString(){
+            return symbol;
+        }
+
+        public String getSymbol() {
+            return symbol;
+        }
+    }
+    
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCurrencyName() {
+        return currencyName;
+    }
+
+    public void setCurrencyName(String currencyName) {
+        this.currencyName = currencyName;
+    }
+
+    public String getCurrencyCode() {
+        return currencyCode;
+    }
+
+    public void setCurrencyCode(String currencyCode) {
+        this.currencyCode = unitySymbol.valueOf(currencyName).getSymbol();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, currencyName, currencyCode);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        final CurrencyRate other = (CurrencyRate) obj;
+        return Objects.equals(this.id, other.id)
+                && Objects.equals(this.currencyName, other.currencyName)
+                && Objects.equals(this.currencyCode, other.currencyCode);
+    }
 }
