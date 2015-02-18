438dfd4da958938eee3abe8e01983d53e9cad89e
AtanvarnoJack <alexcornillon@gmail.com>
2015-02-18 15:47:57 +0100

    getRateGroupByType

--- src/main/java/bigdata/analytics/AnalyticsService.java
+++ src/main/java/bigdata/analytics/AnalyticsService.java
@@ -1,8 +1,24 @@
 package bigdata.analytics;
 
+import bigdata.analytics.rategroup.RateGroup;
+import bigdata.analytics.rategroup.RateGroupType;
+import bigdata.datastorage.dao.rategroup.RateGroupDao;
+
+import javax.inject.Inject;
+import java.util.List;
+
 /**
  * Created by alexandre on 18/02/2015.
  */
 public class AnalyticsService {
-
+    @Inject
+    RateGroupDao rateGroupDao;
+    /***
+     * @param rateGroupType
+     * @return List<RateGroup> en fonction de Param
+     */
+    
+    public List<RateGroup> getRateGroupByType (RateGroupType rateGroupType){
+        return rateGroupDao.getRateGroupsByType(rateGroupType);
+    }
 }
