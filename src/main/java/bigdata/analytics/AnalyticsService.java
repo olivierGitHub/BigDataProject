8dcd1a32c7ee8f9dfec299591be31725fbb9af61
AtanvarnoJack <alexcornillon@gmail.com>
2015-02-18 15:37:53 +0100

    services os

---
+++ src/main/java/bigdata/analytics/AnalyticsService.java
@@ -0,0 +1,8 @@
+package bigdata.analytics;
+
+/**
+ * Created by alexandre on 18/02/2015.
+ */
+public class AnalyticsService {
+
+}
---
+++ src/main/java/bigdata/importation/ImportationService.java
@@ -0,0 +1,7 @@
+package bigdata.importation;
+
+/**
+ * Created by alexandre on 18/02/2015.
+ */
+public class ImportationService {
+}
