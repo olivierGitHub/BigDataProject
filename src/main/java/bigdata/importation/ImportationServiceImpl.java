44e469ff986524604674ec0f92be6e2c14cd91d6
Arnaud <arnaud.lewis@icloud.com>
2015-02-25 00:15:47 +0100

    attention aux conflits !!!!

--- src/main/java/bigdata/importation/ImportationServiceImpl.java
+++ src/main/java/bigdata/importation/ImportationServiceImpl.java
@@ -5,6 +5,7 @@ import jxl.read.biff.BiffException;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class ImportationServiceImpl implements ImportationService {
@@ -24,7 +25,19 @@ public class ImportationServiceImpl implements ImportationService {
             e.printStackTrace();
         }
     }
-    
 
+    public List<String> getYearFileData(File file) {
+        List<String> resultList = new ArrayList<String>();
+        FileReader excelReader = new ExcelReader();
+        try {
+            excelReader.takeReader(file);
+            resultList = excelReader.getAllYears();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (BiffException e) {
+            e.printStackTrace();
+        }
+        return resultList;
+    }
     
 }
